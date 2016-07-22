6fabc574f47dea7f5a7e302a85eee3968ad2329a
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2016-07-21 21:59:08 -0400

    Update to latest version of indicator library
    NumberPickers are initialized to the correct runtime values
    The main screen now calls ::notifyDataSetChanged() after any change is recorded (no throttling yet)

--- mobile/src/main/java/com/cliabhach/emptyhusk/MainActivity.java
+++ mobile/src/main/java/com/cliabhach/emptyhusk/MainActivity.java
@@ -1,8 +1,9 @@
 package com.cliabhach.emptyhusk;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -21,11 +22,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        initButtons(fab);
-
         ViewPager viewPager = (ViewPager) findViewById(R.id.mainViewPager);
-        initPager(viewPager);
+        SimplePagerAdapter adapter = defineAdapter();
+        initPager(viewPager, adapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        initButtons(fab, adapter);
 
         CutoutViewIndicator cvi = (CutoutViewIndicator) findViewById(R.id.cutoutViewIndicator);
         initIndicator(cvi, viewPager);
@@ -33,53 +35,68 @@ public class MainActivity extends AppCompatActivity {
         NumberPicker spacing = (NumberPicker) findViewById(R.id.spacingPicker);
         NumberPicker width = (NumberPicker) findViewById(R.id.widthPicker);
         NumberPicker height = (NumberPicker) findViewById(R.id.heightPicker);
-        initPickers(spacing, width, height, cvi);
+        initPickers(spacing, width, height, cvi, adapter);
     }
 
-    private void initButtons(FloatingActionButton fab) {
+    private void initButtons(FloatingActionButton fab, final @NonNull PagerAdapter adapter) {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                adapter.notifyDataSetChanged();
             }
         });
     }
 
-    private void initPager(ViewPager viewPager) {
-        String[] pages = new String[]{"1", "2", "3", "4", "5", "6"};
-        viewPager.setAdapter(new SimplePagerAdapter(pages));
+    @NonNull
+    private SimplePagerAdapter defineAdapter() {
+        String[] pages = {"1", "2", "3", "4", "5", "6"};
+        return new SimplePagerAdapter(pages);
+    }
+
+    private void initPager(ViewPager viewPager, SimplePagerAdapter adapter) {
+        viewPager.setAdapter(adapter);
     }
 
     private void initIndicator(CutoutViewIndicator cvi, ViewPager viewPager) {
         cvi.setViewPager(viewPager);
     }
 
-    private void initPickers(NumberPicker spacing, NumberPicker width, NumberPicker height, final CutoutViewIndicator cvi) {
+    private void initPickers(
+            NumberPicker spacing, NumberPicker width, NumberPicker height,
+            final CutoutViewIndicator cvi,
+            final PagerAdapter adapter
+    ) {
+        // TODO: throttle calls to notifyDataSetChanged
         setRangeOn(spacing);
+        spacing.setValue(cvi.getInternalSpacing());
         spacing.setOnValueChangedListener(new OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
                 if (newVal >= 0) {
-                    cvi.setMarginLength(newVal);
+                    cvi.setInternalSpacing(newVal);
+                    adapter.notifyDataSetChanged();
                 }
             }
         });
         setRangeOn(width);
+        width.setValue(cvi.getCellLength());
         width.setOnValueChangedListener(new OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
                 if (newVal >= 0) {
                     cvi.setCellLength(newVal);
+                    adapter.notifyDataSetChanged();
                 }
             }
         });
         setRangeOn(height);
+        height.setValue(cvi.getPerpendicularLength());
         height.setOnValueChangedListener(new OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
                 if (newVal >= 0) {
                     cvi.setPerpendicularLength(newVal);
+                    adapter.notifyDataSetChanged();
                 }
             }
         });
@@ -87,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void setRangeOn(NumberPicker picker) {
         picker.setMinValue(0);
-        picker.setMaxValue(100);
+        picker.setMaxValue(200);
     }
 
 }
