6880cf8333d6eb83c28ca302ab21f5a2a863ad83
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2016-07-21 21:23:12 -0400

    Add some basic NumberPickers for testing out different sizes

--- mobile/src/main/java/com/cliabhach/emptyhusk/MainActivity.java
+++ mobile/src/main/java/com/cliabhach/emptyhusk/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.NumberPicker;
+import android.widget.NumberPicker.OnValueChangeListener;
 
 import com.cliabhach.indicator.CutoutViewIndicator;
 
@@ -27,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
 
         CutoutViewIndicator cvi = (CutoutViewIndicator) findViewById(R.id.cutoutViewIndicator);
         initIndicator(cvi, viewPager);
+
+        NumberPicker spacing = (NumberPicker) findViewById(R.id.spacingPicker);
+        NumberPicker width = (NumberPicker) findViewById(R.id.widthPicker);
+        NumberPicker height = (NumberPicker) findViewById(R.id.heightPicker);
+        initPickers(spacing, width, height, cvi);
     }
 
     private void initButtons(FloatingActionButton fab) {
@@ -40,12 +47,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initPager(ViewPager viewPager) {
-        String[] pages = new String[]{"1", "2", "3"};
+        String[] pages = new String[]{"1", "2", "3", "4", "5", "6"};
         viewPager.setAdapter(new SimplePagerAdapter(pages));
     }
 
     private void initIndicator(CutoutViewIndicator cvi, ViewPager viewPager) {
         cvi.setViewPager(viewPager);
+    }
+
+    private void initPickers(NumberPicker spacing, NumberPicker width, NumberPicker height, final CutoutViewIndicator cvi) {
+        setRangeOn(spacing);
+        spacing.setOnValueChangedListener(new OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                if (newVal >= 0) {
+                    cvi.setMarginLength(newVal);
+                }
+            }
+        });
+        setRangeOn(width);
+        width.setOnValueChangedListener(new OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                if (newVal >= 0) {
+                    cvi.setCellLength(newVal);
+                }
+            }
+        });
+        setRangeOn(height);
+        height.setOnValueChangedListener(new OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                if (newVal >= 0) {
+                    cvi.setPerpendicularLength(newVal);
+                }
+            }
+        });
+    }
+
+    private void setRangeOn(NumberPicker picker) {
+        picker.setMinValue(0);
+        picker.setMaxValue(100);
     }
 
 }
