1a672529981985d58d25cae9b9cc7edd61d1810e
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2016-10-28 15:38:56 -0400

    Add all the tv project's testsuite dependencies
    Side effect of using one emulator for all configs: emulator version must
    be at least 21.

---
+++ tv/src/main/java/com/fuzz/emptyhusk/TvMainActivity.java
@@ -0,0 +1,11 @@
+package com.fuzz.emptyhusk;
+
+import android.app.Activity;
+
+/**
+ * Stub Leanback-compatible activity for use on televisions.
+ *
+ * @author Philip Cohn-Cort (Fuzz)
+ */
+public class TvMainActivity extends Activity {
+}
