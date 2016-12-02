ddd7f03fb75fd320fabceea860668ae5caeeaa6e
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2016-12-02 17:59:00 -0500

    Add some new OffSetters methods
    OffSetFlag acts more or less as an enum-like way of
    identifying which of these methods to activate.

---
+++ indicator/src/main/java/com/fuzz/indicator/OffSetFlag.java
@@ -0,0 +1,48 @@
+package com.fuzz.indicator;
+
+import android.graphics.Matrix;
+import android.support.annotation.IntDef;
+import android.view.View;
+import android.widget.ImageView;
+
+import static com.fuzz.indicator.OffSetFlag.ALPHA;
+import static com.fuzz.indicator.OffSetFlag.IMAGE_ALPHA;
+import static com.fuzz.indicator.OffSetFlag.IMAGE_SCALE;
+import static com.fuzz.indicator.OffSetFlag.SCALE;
+import static com.fuzz.indicator.OffSetFlag.IMAGE_TRANSLATE;
+
+/**
+ * @author Philip Cohn-Cort (Fuzz)
+ */
+@IntDef(value =
+        {IMAGE_TRANSLATE,
+                ALPHA, IMAGE_ALPHA,
+                SCALE, IMAGE_SCALE
+        },
+        flag = true)
+public @interface OffSetFlag {
+    /**
+     * Corresponds to {@link OffSetters#offsetImageBy(ImageView, int, float, Matrix)}
+     */
+    int IMAGE_TRANSLATE = 1;
+    /**
+     * Corresponds to {@link OffSetters#offsetAlphaBy(View, float)}
+     * @see #IMAGE_ALPHA
+     */
+    int ALPHA               = 1 << 1;
+    /**
+     * Corresponds to {@link OffSetters#offsetImageAlphaBy(ImageView, float)}
+     * @see #ALPHA
+     */
+    int IMAGE_ALPHA         = 1 << 2;
+    /**
+     * Corresponds to {@link OffSetters#offsetScaleBy(View, float)}
+     * @see #IMAGE_SCALE
+     */
+    int SCALE               = 1 << 3;
+    /**
+     * Corresponds to {@link OffSetters#offsetImageScaleBy(ImageView, float)}
+     * @see #SCALE
+     */
+    int IMAGE_SCALE         = 1 << 4;
+}
