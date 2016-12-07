de62189fa5038de8b13831f306904eaa233c482e
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2016-12-06 22:15:50 -0500

    Make UnDrawable much simpler - no need for interception or proxies

--- indicator/src/main/java/com/fuzz/indicator/widget/UnDrawable.java
+++ indicator/src/main/java/com/fuzz/indicator/widget/UnDrawable.java
@@ -1,13 +1,12 @@
 package com.fuzz.indicator.widget;
 
-import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Region;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.DrawableContainer;
-import android.graphics.drawable.LevelListDrawable;
-import android.support.annotation.NonNull;
+import android.graphics.drawable.StateListDrawable;
 import android.support.annotation.Nullable;
+import android.util.StateSet;
 
 /**
  * This class wraps a drawable, exposing (almost) all of its properties.
@@ -15,42 +14,7 @@ import android.support.annotation.Nullable;
  *
  * @author Philip Cohn-Cort (Fuzz)
  */
-public class UnDrawable extends DrawableContainer {
-
-    /**
-     * Note that the constructor for {@link android.graphics.drawable.DrawableContainer.DrawableContainerState}
-     * is not publicly accessible. This field is a copy of the DrawableContainerState
-     * most recently set on the anonymous LevelListDrawable in {@link #UnDrawable()}.
-     */
-    private DrawableContainerState extracted;
-
-    /**
-     * Create a new {@link UnDrawable} with a state stolen from {@link LevelListDrawable}.
-     */
-    @SuppressWarnings("deprecation")
-    public UnDrawable() {
-        Drawable seed = Resources.getSystem().getDrawable(android.R.drawable.btn_plus);
-        new LevelListDrawable() {
-            @Override
-            protected void setConstantState(@NonNull DrawableContainerState state) {
-                super.setConstantState(state);
-                extracted = state;
-            }
-        };
-        extracted.addChild(seed);
-        setConstantState(extracted);
-        setInternalDrawable(seed);
-    }
-
-    @Override
-    public int getChangingConfigurations() {
-        int changingConfigurations = 0;
-        // The superclass will query the constant state, so be careful around that.
-        if (extracted != null) {
-            changingConfigurations = super.getChangingConfigurations();
-        }
-        return changingConfigurations;
-    }
+public class UnDrawable extends StateListDrawable {
 
     /**
      * The parameter to this method may later be retrieved with {@link #getCurrent()}.
@@ -61,20 +25,13 @@ public class UnDrawable extends DrawableContainer {
      *                            about this drawable (e.g. size, transparency,
      *                            etc.).
      */
-    public void setInternalDrawable(@NonNull Drawable internalDrawable) {
-        if (extracted == null) {
+    public void setInternalDrawable(Drawable internalDrawable) {
+        if (internalDrawable == null) {
             throw new IllegalStateException(
                     "\"setInternalDrawable\" may only be called on UnDrawables with non-null resources."
             );
         }
-        if (extracted.getChildCount() <= 0) {
-            // We need the state to expand its child array for us
-            extracted.addChild(internalDrawable);
-        } else {
-            // We can modify the array directly.
-            Drawable[] children = extracted.getChildren();
-            children[0] = internalDrawable;
-        }
+        addState(StateSet.WILD_CARD, internalDrawable);
 
         selectDrawable(0);
     }
@@ -88,18 +45,6 @@ public class UnDrawable extends DrawableContainer {
         } else {
             return super.getTransparentRegion();
         }
-    }
-
-    /**
-     * For some weird reason, this method is normally protected from objects outside
-     * of the DrawableContainer (sub)class. So much of the drawable subsystem is just
-     * really clandestine.
-     *
-     * @param state    where the contained drawables are kept.
-     */
-    @Override
-    public void setConstantState(DrawableContainerState state) {
-        super.setConstantState(state);
     }
 
     /**
