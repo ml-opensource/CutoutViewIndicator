2c1bdf3d9b382dc6bc9e626895f11ea47c88e7ca
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2016-12-08 17:46:22 -0500

    Allow users to set OffsetHints directly on CutoutViewIndicator instances.
    Essential for issue #59

--- indicator/src/main/java/com/fuzz/indicator/OffSetHint.java
+++ indicator/src/main/java/com/fuzz/indicator/OffSetHint.java
@@ -1,9 +1,27 @@
+/*
+ * Copyright 2016 Philip Cohn-Cort
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.fuzz.indicator;
 
 import android.graphics.Matrix;
 import android.support.annotation.IntDef;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 
 import static com.fuzz.indicator.OffSetHint.ALPHA;
 import static com.fuzz.indicator.OffSetHint.IMAGE_ALPHA;
@@ -12,6 +30,16 @@ import static com.fuzz.indicator.OffSetHint.SCALE;
 import static com.fuzz.indicator.OffSetHint.IMAGE_TRANSLATE;
 
 /**
+ * Like an enum, but not. The int values in this class correspond
+ * directly to methods in {@link OffSetters}.
+ * <ul>
+ *     <li>{@link #IMAGE_TRANSLATE}</li>
+ *     <li>{@link #ALPHA}</li>
+ *     <li>{@link #IMAGE_ALPHA}</li>
+ *     <li>{@link #SCALE}</li>
+ *     <li>{@link #IMAGE_SCALE}</li>
+ * </ul>
+ *
  * @author Philip Cohn-Cort (Fuzz)
  */
 @IntDef(value =
@@ -20,6 +48,7 @@ import static com.fuzz.indicator.OffSetHint.IMAGE_TRANSLATE;
                 SCALE, IMAGE_SCALE
         },
         flag = true)
+@Retention(RetentionPolicy.CLASS)
 public @interface OffSetHint {
     /**
      * Corresponds to {@link OffSetters#offsetImageBy(ImageView, int, float, Matrix)}
