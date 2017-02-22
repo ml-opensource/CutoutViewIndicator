8ab31f91a7959f6e515fa1eeaaa6f84c00dbcd0b
Philip Cohn-Cort <pcohn-cort@fuzzproductions.com>
2017-02-22 14:32:02 -0500

    Support reading text paths directly onto a TextClippedImageView

--- indicator/src/main/java/com/fuzz/indicator/widget/TextClippedImageView.java
+++ indicator/src/main/java/com/fuzz/indicator/widget/TextClippedImageView.java
@@ -17,11 +17,13 @@ package com.fuzz.indicator.widget;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
@@ -30,6 +32,13 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+
+import com.fuzz.indicator.R;
+
+import static android.view.View.MeasureSpec.AT_MOST;
+import static android.view.View.MeasureSpec.getMode;
+import static android.view.View.MeasureSpec.getSize;
+import static android.view.View.MeasureSpec.makeMeasureSpec;
 
 /**
  * Custom variant of {@link ClippedImageView} that supports an additional mask,
@@ -67,30 +76,59 @@ public class TextClippedImageView extends ClippedImageView {
     @NonNull
     protected TextPaint textPaint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
 
+    @NonNull
+    protected final RectF pathBounds = new RectF();
+
+    /**
+     * The number of custom paths in use. For now, this is 0 by default, 1 if either
+     * {@link #backgroundMaskPath} or {@link #backgroundMaskText} is set.
+     */
+    protected int customPathCount;
+
+    /**
+     * Flag indicating whether the current {@link #backgroundMaskPath} has been translated
+     * into the center of this view.
+     *
+     * @see #clipImageInto(int, int)
+     * @see #extensibleOnMeasure(int, int)
+     */
+    protected boolean hasOffset;
+
     public TextClippedImageView(Context context) {
         super(context);
-        init(context);
+        init(context, null, 0, 0);
     }
 
     public TextClippedImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init(context, attrs, 0, 0);
     }
 
     public TextClippedImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
+        init(context, attrs, defStyleAttr, 0);
     }
 
     @SuppressWarnings("unused")
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public TextClippedImageView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        init(context);
+        init(context, attrs, defStyleAttr, defStyleRes);
     }
 
-    protected void init(@NonNull Context context) {
+    protected void init(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         resetTextPaint();
+
+        String textMask = null;
+
+        if (attrs != null) {
+            TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TextClippedImageView, defStyleAttr, defStyleRes);
+
+            textMask = a.getString(R.styleable.TextClippedImageView_clip_text_mask);
+
+            a.recycle();
+        }
+        setTextMaskPath(textMask);
     }
 
     /**
@@ -123,6 +161,52 @@ public class TextClippedImageView extends ClippedImageView {
         setTextMaskPath(backgroundMaskText);
     }
 
+    @Override
+    protected void extensibleOnMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthMode = getMode(widthMeasureSpec);
+        int heightMode = getMode(heightMeasureSpec);
+
+        int currentWidth = getSize(widthMeasureSpec);
+        int currentHeight = getSize(heightMeasureSpec);
+
+        if (backgroundMaskPath != null) {
+            hasOffset = false;
+            // Unconditionally compute path bounds - we'll want them in ::clipImageInto
+            backgroundMaskPath.computeBounds(pathBounds, false); // second param is irrelevant
+
+            if (customPathCount > 0) {
+                // Path size can take precedence
+                float proposedWidth = pathBounds.width() - pathBounds.left;
+                float proposedHeight = pathBounds.height() - pathBounds.top;
+                if (isReasonableProposal(heightMode, currentHeight, proposedHeight)) {
+                    heightMeasureSpec = makeMeasureSpec((int) proposedHeight, heightMode);
+                }
+                if (isReasonableProposal(widthMode, currentWidth, proposedWidth)) {
+                    widthMeasureSpec = makeMeasureSpec((int) proposedWidth, widthMode);
+                }
+            }
+        }
+        super.extensibleOnMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    /**
+     * Sanity check to be used right before modifying width or height.
+     *
+     * @param specMode        a mode returned by {@link MeasureSpec#getMode}
+     * @param specSize        a size returned by {@link MeasureSpec#getMode}
+     * @param proposedSize    a new value to replace {@code specSize}
+     * @return true if {@code proposedSize} could be used, false otherwise.
+     * @see #extensibleOnMeasure(int, int)
+     */
+    private boolean isReasonableProposal(int specMode, int specSize, float proposedSize) {
+        return proposedSize >= 1 &&
+                (
+                    specMode != AT_MOST
+                            ||
+                    proposedSize < specSize
+                );
+    }
+
     @NonNull
     @Override
     protected Bitmap clipImageInto(int viewWidth, int viewHeight) {
@@ -136,6 +220,10 @@ public class TextClippedImageView extends ClippedImageView {
         textPaint.setXfermode(null);
 
         if (backgroundMaskPath != null) {
+            if (!hasOffset && (pathBounds.left != 0 || pathBounds.top != 0)) {
+                hasOffset = true;
+                backgroundMaskPath.offset(-pathBounds.left, -pathBounds.top);
+            }
             canvas.drawPath(backgroundMaskPath, textPaint);
             paint.setXfermode(overlayTransferMode);
         } else if (backgroundMaskText != null) {
@@ -167,10 +255,16 @@ public class TextClippedImageView extends ClippedImageView {
             if (backgroundMaskPath == null) {
                 backgroundMaskPath = new Path();
             }
-            textPaint.getTextPath(mask, 0, mask.length(), getWidth() / 2, getHeight() / 2, backgroundMaskPath);
+            // Create the path using a 0,0 origin (top-left) - it'll be centred later
+            textPaint.getTextPath(mask, 0, mask.length(), 0, 0, backgroundMaskPath);
+            customPathCount = 1;
         } else {
             backgroundMaskPath = null;
         }
+    }
+
+    public boolean hasTextMask() {
+        return backgroundMaskText != null;
     }
 
     @Override
